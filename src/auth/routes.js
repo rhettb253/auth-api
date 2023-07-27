193864da5d3940e0a1d5dc15d20aea88de66b91b
Rhett Beardemphl <rhettbeardemphl@hotmail.com>
2023-07-26 17:53:33 -0700

    fixing routes

--- src/auth/routes.js
+++ src/auth/routes.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const authRouter = express.Router();
 
-const { users } = require('./models');
+const { users } = require('../models/index.js');
 const basicAuth = require('./middleware/basic.js')
 const bearerAuth = require('./middleware/bearer.js')
 const permissions = require('./middleware/acl.js')
--- src/server.js
+++ src/server.js
@@ -4,23 +4,25 @@
 const express = require('express');
 const cors = require('cors');
 
-
 // Esoteric Resources
 const errorHandler = require('./error-handlers/500.js');
 const notFound = require('./error-handlers/404.js');
 const authRoutes = require('./auth/routes.js');
+const v1Routes = require('./routes/v1.js');
+const logger = require('./middleware/logger.js');
 
 // Prepare the express app
 const app = express();
 
 // App Level MW
 app.use(cors());
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(logger);
 
 // Routes
 app.use(authRoutes);
+app.use('/api/v1', v1Routes);
 
 // Catchalls
 app.use(notFound);
